1a5ab1be4739ad9567193edcd2371400b73b2946
thanghq99 <haikhauak@gmail.com>
2020-11-25 20:07:57 +0000

    Add name age to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_170315) do
-ActiveRecord::Schema.define(version: 2020_11_13_182300) do
+ActiveRecord::Schema.define(version: 2020_11_14_015218) do
+
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,15 +41,9 @@ ActiveRecord::Schema.define(version: 2020_11_13_182300) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "images", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "title"
-    t.string "file"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -58,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_11_13_182300) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
